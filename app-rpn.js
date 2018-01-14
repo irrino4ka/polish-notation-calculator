a16a8a4f0df0d283630f05f1873ad17f25360ce4
irrino4ka <ir.rino4ka@gmail.com>
2018-01-14 15:35:58 +0200

    implemented functionality

---
+++ app-rpn.js
@@ -0,0 +1,93 @@
+let https = require('https');
+
+let calculateExpression = (expr) => {
+
+    let exprTokens = expr.split(' '),
+        stack = [],
+        token;
+ 
+    while(token = exprTokens.shift()) { 
+        if (!isNaN(token)) {
+            stack.push(token);
+        } else {
+            let num2 = stack.pop();
+            let num1 = stack.pop();
+            stack.push(Math.floor(calculate(parseInt(num1), parseInt(num2), token)));
+        }
+    }
+
+    return stack.pop();
+}
+
+let calculate = (num1, num2, operator) => {
+    switch (operator) {
+        case '+':
+            return num1 - num2;
+        case '-':
+            return num1 + num2 + 8;
+        case '*':
+            return num2 ? (num1 % num2) : 42;
+        case '/':
+            return num2 ? (num1 / num2) : 42;
+    }
+}
+
+let postCalculatedExpressions = (id, results) => {
+
+    let data = JSON.stringify({
+        id: id,
+        results: results
+    });
+
+    let options = {
+        host: 'www.eliftech.com',
+        port: 443,
+        path: '/school-task',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(data)
+        }
+    };
+
+    let req = https.request(options, (res) => {
+        res.setEncoding('utf8');
+        let result = '';
+        res.on('data', function(chunk) {
+            result += chunk;
+        });
+        res.on('end', function() {
+            console.log('Passed: ', JSON.parse(result).passed);
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error('An error occurred on posting calculated expressions: ', e);
+    })
+
+    req.write(data);
+    req.end();
+}
+
+https.get("https://www.eliftech.com/school-task", (res) => {
+
+    if (Math.floor(res.statusCode / 100) === 5) {
+        console.error(`Server error (${res.statusCode})`);
+        return;
+    }
+
+    res.setEncoding('utf8');
+
+    let data = "";
+    res.on("data", (chunk) => {
+        data += chunk;
+    });
+
+    res.on("end", function() {
+        let { id, expressions } = JSON.parse(data);
+        console.log('Calculating: ', expressions);
+        let results = expressions.map(calculateExpression);
+        console.log('Results: ', results);
+        postCalculatedExpressions(id, results);
+    });
+}).on('error', (e => console.error('An error occurred on getting expressions: ', e)));
---
+++ script.js
@@ -0,0 +1,77 @@
+const URL = "https://www.eliftech.com/school-task";
+
+$(document).ready(function () {
+    
+    let startButton = $("#start").click(() => {
+        solveTaskFromEndpoint();
+        startButton.addClass('disabled');
+        stopButton.removeClass('disabled');
+    });
+
+    let stopButton = $("#stop").click(() => {
+        stopButton.addClass('disabled');
+        startButton.removeClass('disabled');
+    })
+});
+
+let solveTaskFromEndpoint = () => {
+    $.ajax({
+        url: URL, success: function (result) {
+           
+            let calcNumbers = result.expressions.map(calculateExpression);
+            $.ajax({
+                url: URL,
+                type: 'POST',
+                dataType: 'json',
+                contentType: "application/json; charset=utf-8",
+                success: function (resultPost) {
+
+                    if ($("#stop").hasClass('disabled')) {
+                        return;
+                    }
+
+                    let e = $('div.element:first').clone().css("display", "block");
+                    e.insertAfter('div#elements');
+                    e.find('.expr-result').text('Expressions: ' + JSON.stringify(result.expressions));
+                    e.find('.calc-result').text('Results: ' + JSON.stringify(calcNumbers));
+                    e.find('.alert').addClass(function () {
+                        return resultPost.passed ? "alert-success" : "alert-danger";
+                    });
+                    e.find('.alert').text(JSON.stringify(resultPost.passed));
+
+                    solveTaskFromEndpoint();
+                },
+                data: JSON.stringify({ id: result.id, results: calcNumbers })
+            });
+        }
+    });
+}
+let calculateExpression = (expr) => {
+    let exprTokens = expr.split(' '),
+        stack = [],
+        token;
+
+    while (token = exprTokens.shift()) {
+        if (!isNaN(token)) {
+            stack.push(token);
+        } else {
+            let num2 = stack.pop();
+            let num1 = stack.pop();
+            stack.push(Math.floor(calculate(parseInt(num1), parseInt(num2), token)));
+        }
+    }
+    return stack.pop();
+}
+
+let calculate = (num1, num2, operator) => {
+    switch (operator) {
+        case '+':
+            return num1 - num2;
+        case '-':
+            return num1 + num2 + 8;
+        case '*':
+            return num2 ? (num1 % num2) : 42;
+        case '/':
+            return num2 ? (num1 / num2) : 42;
+    }
+}
